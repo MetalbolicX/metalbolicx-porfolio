85cb3649c4e90ae94f6c9e23fcd7e4aa1d3d1067
José Martínez Santana <jose.martinez.santana@gmail.com>
2023-06-13 10:01:46 -0600

    Added the portfolio, about and contact HTML

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,6 +1,8 @@
 import createHome from "../pages/home";
 import createContact from "../pages/contact";
+import createAbout from "../pages/about";
 import createError404 from "../pages/error404";
+import createPortfolio from "../pages/portfolio";
 import getHash from "../utils/gethash";
 import resolveRoutes from "../utils/resolveroutes";
 import { sectionsButtons } from "../utils/constants";
@@ -14,25 +16,28 @@ const routes: Routes = {
   "/": createHome,
   "/home": createHome,
   "/contact": createContact,
+  "/about": createAbout,
+  "/portfolio": createPortfolio,
 };
 
 async function router() {
+  // Get the path of the url
   const hash: string = getHash();
   const route: string = resolveRoutes(hash);
   const render: RouteHandler = routes[route] || createError404;
-  console.log(route);
-  // Delete previous info
+  // Delete previous info of the DOM
   sectionsButtons.forEach((section: string) => {
     const container = document.getElementById(section);
     while (container?.firstChild) {
       container.firstChild.remove();
     }
   });
-
-  const contentAdder: HTMLElement | null =
+  // Add the information for the section selected
+  const content: HTMLElement | null =
     null ||
-    document.getElementById(route.length === 1 ? "home" : route.slice(1));
-  contentAdder?.append(await render());
+    document.getElementById(route === "/" ? "home" : route.slice(1));
+  content?.append(await render());
+
 }
 
 export default router;
