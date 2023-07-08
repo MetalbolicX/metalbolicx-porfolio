886d0d8933e6e5094b931a3856504eb8238ac4b6
José Martínez Santana <jose.martinez.santana@gmail.com>
2023-07-08 09:10:07 -0600

    Change of the function which gets the hash to parse to trim the backslash if exist

--- src/utils/getpath.ts
+++ src/utils/getpath.ts
@@ -1,7 +1,9 @@
 /**
  * Get the hash from  window current url and parse it.
  */
-const getPath = (): string =>
-  location.hash.slice(1, -1).toLocaleLowerCase() || "/";
+function getPath(): string {
+  const urlHash: string = window.location.hash;
+  return urlHash.at(-1) === "/" ? urlHash.slice(1, -1) : urlHash.slice(1) || "/";
+}
 
 export default getPath;
