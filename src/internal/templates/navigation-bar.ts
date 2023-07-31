814550d4fa4f0b56be3cceb3b5badb084704a5bc
José Martínez Santana <jose.martinez.santana@gmail.com>
2023-07-31 12:17:11 -0600

    Refactor the coloring of the bottom using the pathname of the browser API

--- src/internal/templates/navigation-bar.ts
+++ src/internal/templates/navigation-bar.ts
@@ -6,6 +6,12 @@ import { sections, navIcons } from "../utils/constants";
  * @returns The array of HTML elements buttons for the navigation bar.
  */
 function addNavigationBar(): HTMLElement[] {
+  // Find the section to color the bottom of the navigation bar.
+  let sectionTocolor: number = sections.findIndex(
+      (section: string) => window.location.pathname.includes(section)
+    );
+  // In case the root path of the application
+  sectionTocolor = sectionTocolor < 0 ? 0 : sectionTocolor;
   const buttons: HTMLElement[] = sections.map((section: string, index: number) => {
     const button: DOMNode = new DOMNode(
       "div",
@@ -15,7 +21,7 @@ function addNavigationBar(): HTMLElement[] {
           <i class="fas fa-${navIcons.at(index)}"></i>
         </a>
       `,
-      index === 0 ? ["control", "active-btn"] : ["control"]
+      index === sectionTocolor ? ["control", "active-btn"] : ["control"]
     );
     button.setAttribute("data-id", section);
     return button.getNode;
