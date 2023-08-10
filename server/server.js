d97ceb4ea25877aec808f570af005846b48861c2
José Martínez Santana <jose.martinez.santana@gmail.com>
2023-08-10 06:36:53 -0600

    Refactor of the code in case the file is not in the cache to show the error 404

--- server/server.js
+++ server/server.js
@@ -55,7 +55,8 @@ class StaticFileServer {
           const fileContent = fs.readFileSync(fullPath, "utf8");
           return {
             ...aggregated,
-            [extension]: [ // Create a list of object for each file and its content
+            [extension]: [
+              // Create a list of object for each file and its content
               ...fileNames,
               { filename: name, filecontent: fileContent },
             ],
@@ -94,24 +95,8 @@ class StaticFileServer {
       res.writeHead(200, { "Content-Type": contentType });
       res.end(cachedContent.filecontent, "utf-8");
     } else {
-      const filePath = path.join(this.staticPath, parsedUrl);
-      const contentType = this.#getContentType(filePath);
-
-      fs.readFile(filePath, (err, content) => {
-        if (err) {
-          if (err.code === "ENOENT") {
-            res.writeHead(404, { "Content-Type": "text/plain" });
-            res.end("File not found");
-          } else {
-            res.writeHead(500, { "Content-Type": "text/plain" });
-            res.end("Server error");
-          }
-        } else {
-          this.#fileCache[parsedUrl] = content; // Cache the content
-          res.writeHead(200, { "Content-Type": contentType });
-          res.end(content, "utf-8");
-        }
-      });
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end("File not found");
     }
   }
 
